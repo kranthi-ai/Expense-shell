81a394f72f5c1d6e361aac561fdc38a6bd4acf3f
kranthi-ai <kranthi.k1810@gmail.com>
2025-01-21 09:29:17 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -51,7 +51,7 @@ else
 
  fi
 
-mkdir /app
+mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
