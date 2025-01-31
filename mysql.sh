d65378054467e28409901e4043aa5fd25576c66e
kranthi-ai <kranthi.k1810@gmail.com>
2025-01-31 17:12:51 +0530

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -42,7 +42,7 @@ systemctl start mysqld   &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
 mysql -h mysql.kkdevops.site -u root -pExpenseApp@1 -e 'show databases;' 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "Mysql Root password not setup"  &>>$LOG_FILE_NAME
     mysql_secure_installation --set-root-pass ExpenseApp@1
